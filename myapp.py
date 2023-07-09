0f890a7d1ae2a070d0646399f842bc6f070560cd
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-09 12:37:59 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -1,12 +1,12 @@
 from yahoofinancials import YahooFinancials
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 st.write("""
 # Simple Stock Price App
 
-Enter the ticker symbol to see the stock **closing price** and **volume** of the stock!
+Enter the ticker symbol to see the stock **closing price** and **volume**!
+
 """)
 
 # Ask user for ticker symbol
@@ -22,25 +22,20 @@ if tickerSymbol:
         tickerDf = tickerData.get_historical_price_data(start_date='2010-05-31', end_date='2020-05-31', time_interval='daily')
 
         # Extract the historical stock prices
-        if tickerSymbol in tickerDf:
-            prices = tickerDf[tickerSymbol]['prices']
-            df = pd.DataFrame(prices)
-            df['date'] = pd.to_datetime(df['formatted_date'])
+        prices = tickerDf[tickerSymbol]['prices']
+        df = pd.DataFrame(prices)
+        df['date'] = pd.to_datetime(df['formatted_date'])
 
-            # Convert 'close' column to list
-            close_values = df['close'].tolist()
+        st.write("""
+        ## Closing Price
+        """)
+        st.line_chart(df['close'])
 
-            st.write("""
-            ## Closing Price
-            """)
-            st.line_chart(close_values)
+        st.write("""
+        ## Volume Price
+        """)
+        st.line_chart(df['volume'])
 
-            st.write("""
-            ## Volume Price
-            """)
-            st.line_chart(df['volume'])
-        else:
-            st.write("Invalid ticker symbol. Please enter a valid symbol.")
     except KeyError:
         st.write("Invalid ticker symbol. Please enter a valid symbol.")
 
