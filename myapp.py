44a08bfd94f8cc221942c526fc336c07ad0017c4
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-18 00:47:40 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import yahoofinancials as yf
+import yfinance as yf
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta, date as dt_date
 from statsmodels.tsa.arima.model import ARIMA
