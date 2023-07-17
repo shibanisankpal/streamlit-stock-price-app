7ab56a74ffa0b8718adaf7445b4d45193f35f4fd
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-18 00:37:52 +0800

    commit

--- myapp.py
+++ myapp.py
@@ -1,81 +1,80 @@
-from yahoofinancials import YahooFinancials
 import streamlit as st
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+import yfinance as yf
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta, date as dt_date
+from statsmodels.tsa.arima.model import ARIMA
+import numpy as np
 
-st.write("""
-# Stock Price Prediction
+def get_stock_data(symbol, start_date, end_date):
+    stock_data = yf.download(symbol, start=start_date, end=end_date)
+    return stock_data
 
-Enter the ticker symbol to predict the future stock **closing price**!
-""")
+def get_company_name(symbol):
+    company = yf.Ticker(symbol)
+    return company.info['longName']
 
-# Ask user for ticker symbol
-tickerSymbol = st.text_input("Enter ticker symbol")
 
-# Check if ticker symbol is provided
-if tickerSymbol:
-    try:
-        # Get data on the ticker symbol
-        tickerData = YahooFinancials(tickerSymbol)
+def train_arima_model(data, prediction_days):
+    model = ARIMA(data['Adj Close'], order=(5, 1, 0))
+    model_fit = model.fit()
 
-        # Get the historical prices for this ticker
-        tickerDf = tickerData.get_historical_price_data(start_date='2010-05-31', end_date='2023-05-31', time_interval='daily')
+    # Get the forecasted values (including the last actual price)
+    forecast = model_fit.forecast(steps=prediction_days)
 
-        # Extract the historical stock prices
-        prices = tickerDf[tickerSymbol]['prices']
-        df = pd.DataFrame(prices)
-        df['date'] = pd.to_datetime(df['formatted_date'])
-        df = df[['date', 'close']]
-        df = df.set_index('date')
 
-        # Create a new DataFrame with a shifted column for the target variable
-        df_shifted = df.shift(-1)
+    # Prepare data for prediction
+    last_date = data.index[-1]
+    forecast_dates = pd.date_range(start=last_date + timedelta(days=1), periods=prediction_days)
 
-        # Split the data into training and testing sets
-        train_data = df.iloc[:-100]  # Use the first n-100 data points for training
-        test_data = df.iloc[-100:]  # Use the last 100 data points for testing
+    return model_fit, forecast, forecast_dates
 
-        # Scale the data
-        scaler = MinMaxScaler()
-        train_scaled = scaler.fit_transform(train_data)
-        test_scaled = scaler.transform(test_data)
 
-        # Split the data into input features and target variable
-        X_train, y_train = train_scaled[:-1, :], train_scaled[1:, -1]
-        X_test, y_test = test_scaled[:-1, :], test_scaled[1:, -1]
 
-        # Reshape the input features to be 3-dimensional for LSTM model
-        X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
-        X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
+def main():
+    st.title("Finance Project with Streamlit")
 
-        # Create an LSTM model
-        model = Sequential()
-        model.add(LSTM(64, activation='relu', input_shape=(X_train.shape[1], 1)))
-        model.add(Dense(1))
-        model.compile(optimizer='adam', loss='mse')
+    # Get user inputs
+    symbol = st.text_input("Enter the stock symbol (e.g., AAPL):", "AAPL")
+    start_date = st.date_input("Start date:", datetime(2021, 1, 1))
 
-        # Train the model
-        model.fit(X_train, y_train, epochs=10, batch_size=32)
+    # Set end_date to today
+    end_date = dt_date.today()  # Use dt_date to avoid conflicts
 
-        # Make predictions on the test set
-        predictions = model.predict(X_test)
-        predictions = scaler.inverse_transform(pd.concat([pd.DataFrame(X_test[:, 0, :-1].reshape(X_test.shape[0], -1)), pd.DataFrame(predictions)], axis=1))[:, -1]
+    # Fetch and display stock data
+    stock_data = get_stock_data(symbol, start_date, end_date)
+    company_name = get_company_name(symbol)
 
-        # Create a new DataFrame for the predictions
-        predictions_df = pd.DataFrame(predictions, columns=['predicted_close'], index=test_data.index[:-1])
+    st.subheader(f"{company_name} ({symbol}) Stock Prices from {start_date} to {end_date}")
+    st.line_chart(stock_data["Adj Close"])
 
-        # Merge the predictions with the test_data DataFrame
-        test_data = test_data.merge(predictions_df, left_index=True, right_index=True, how='outer')
+    # Stock price prediction using ARIMA
+    st.subheader("Stock Price Prediction using ARIMA")
+    prediction_days = st.slider("Select the number of days for prediction:", 1, 30, 7)
 
-        st.write("""
-        ## Actual vs. Predicted Closing Price
-        """)
-        st.line_chart(test_data[['close', 'predicted_close']])
+    # Train the ARIMA model
+    model_fit, forecast, forecast_dates = train_arima_model(stock_data, prediction_days)
 
-    except KeyError:
-        st.write("Invalid ticker symbol. Please enter a valid symbol.")
+    # Plot predicted stock prices using Matplotlib
+    plt.figure(figsize=(10, 6))
+    plt.plot(forecast_dates, forecast, label='Predicted Stock Prices', color='orange')
+    plt.xlabel('Date')
+    plt.ylabel('Stock Price')
+    plt.title(f"Predicted Stock Prices for {company_name} ({symbol})")
+    plt.legend()
+
+    # Display predicted prices as text labels on the plot
+    for date, price in zip(forecast_dates, forecast):
+        plt.text(date, price, f'{price:.2f}', ha='center', va='bottom', color='black')
+
+    st.pyplot(plt)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
